4b87267a49c463157a2f2d496171f379136ff107
mankeheaven <1617615637@qq.com>
2021-08-20 02:06:54 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # oss-publish
-publish static resources to oss
+发布静态资源到oss上，支持命令行方式，或者调用主程序入口
+
+目前仅支持阿里云oss, 计划支持华为云obs，腾讯云等
+
+### 如何使用
+
+1. 安装
+```
+npm install @mankeheaven/oss-publish --save-dev
+```
+
+2. 命令行发布使用方式
+编写配置文件oss-publish.json， 目前仅支持json配置文件
+```
+{
+  "type": "ali",
+  "bucket": "your bucket name",
+  "region": "your region", //例如：oss-cn-shanghai
+  "baseDir": "build", //你要发布的目录，dist or build or other...
+  "defaultWebsiteEntry": "index.html" //如果有html，默认首页入口
+}
+```
+或者配置文件改名字，xxx.json
+
+编写scripts脚本,如果你的配置名称就是oss-publish.json，可以省略--config
+```
+oss-publish --config xxx.json
+```
+
+**另外，需要access_key_id，access_key_secret**，这两者会优先从process.env上拿，如果你想通过ci来发布，可以在ci上设置这两个值为环境变量
+
+如果你想本地发布，可以配置~/.fcli/config.yaml文件, 优先级低于环境变量
+```
+access_key_id: "your ak id"
+access_key_secret: "your ak seceret"
+```
+
+3. 程序使用方式
+```
+import ossPublish from '@mankeheaven/oss-publish';
+
+ossPublish({
+  "type": "ali",
+  "accessKeyId": "your ak id",
+  "accessKeySecret": "your ak secret",
+  "bucket": "your bucket name",
+  "region": "your region", //例如：oss-cn-shanghai
+  "baseDir": "build", //你要发布的目录，dist or build or other...
+  "defaultWebsiteEntry": "index.html" //如果有html，默认首页入口
+})
+
+```
+### TODO
+- [ ] 支持ts
+- [ ] schema做验证
+- [ ] 更多云平台oss发布
--- bin/index.js
+++ bin/index.js
@@ -1,55 +1,63 @@
 #!/usr/bin/env node
-import ossPublish from '../lib/index.js';
-import untildify from 'untildify';
-import fs from 'fs-extra';
-import path from 'path';
-import {Command} from 'commander/esm.mjs'
-import YAML from 'yaml';
+import ossPublish from "../lib/index.js";
+import untildify from "untildify";
+import fs from "fs-extra";
+import path from "path";
+import { Command } from "commander/esm.mjs";
+import YAML from "yaml";
 
 const program = new Command();
 
 // 只支持json配置文件
 // 处理参数 oss-publish --config=filePath(没有就找oss-publish.json)
-// access_key_id，access_key_secret先从process.env上拿，拿不到从 ~/fcli/config.yaml拿
+// access_key_id，access_key_secret先从process.env上拿，拿不到从 ~/.fcli/config.yaml拿
 // 其余的从 oss-publish.json中拿，bucket参数
 
 //命令行 oss-publish
-function getAk(){
+function getAk() {
   let akId = process.env.access_key_id;
   let akSecret = process.env.access_key_secret;
 
-  if(!akId || !akSecret){
-    const credentialsFile= untildify("~/.fcli/config.yaml");
-    if(!fs.existsSync(credentialsFile)){
-      console.error('\x1b[31m','can not find access_key_id and access_key_secret, check if it includes in process.env or ~/fcli/config.yaml','\x1b[m');
+  if (!akId || !akSecret) {
+    const credentialsFile = untildify("~/.fcli/config.yaml");
+    if (!fs.existsSync(credentialsFile)) {
+      console.error(
+        "\x1b[31m",
+        "can not find access_key_id and access_key_secret, check if it includes in process.env or ~/fcli/config.yaml",
+        "\x1b[m"
+      );
       process.exit(1);
     }
     const credentials = parseCredentials(credentialsFile);
-    if(!credentials.accessKeyId || !credentials. accessKeySecret){
-      console.error('\x1b[31m','can not find access_key_id and access_key_secret, check if it includes in process.env or ~/fcli/config.yaml','\x1b[m');
+    if (!credentials.accessKeyId || !credentials.accessKeySecret) {
+      console.error(
+        "\x1b[31m",
+        "can not find access_key_id and access_key_secret, check if it includes in process.env or ~/fcli/config.yaml",
+        "\x1b[m"
+      );
       process.exit(1);
     }
-    akId = credentials.accessKeyId
-    akSecret = credentials.accessKeySecret
+    akId = credentials.accessKeyId;
+    akSecret = credentials.accessKeySecret;
   }
   return {
-    akId, akSecret
-  }
-}
-
-function parseCredentials(file) {
-  const content = fs.readFileSync(file, 'utf8');
-  const doc = YAML.parse(content);
-  return {
-      accessKeyId: doc.access_key_id,
-      accessKeySecret: doc.access_key_secret
+    akId,
+    akSecret,
   };
 }
 
-function getOssOptions (){
+function parseCredentials(file) {
+  const content = fs.readFileSync(file, "utf8");
+  const doc = YAML.parse(content);
+  return {
+    accessKeyId: doc.access_key_id,
+    accessKeySecret: doc.access_key_secret,
+  };
+}
+
+function getOssOptions() {
   //region, accessKeyId, accessKeySecret, bucket
-  program
-  .option('-c, --config', 'config file path')
+  program.option("-c, --config", "config file path");
 
   program.parse(process.argv);
 
@@ -57,63 +65,67 @@ function getOssOptions (){
   let configPath = null;
 
   //不在参数上设置配置文件路径，就找一次默认文件
-  if(!options.config){
-    const defaultConfigPath = path.join(process.cwd(), "oss-publish.json")
-    if(!fs.existsSync(defaultConfigPath)){
-      console.error('\x1b[31m','can not find config file oss-publish.json','\x1b[m');
+  if (!options.config) {
+    const defaultConfigPath = path.join(process.cwd(), "oss-publish.json");
+    if (!fs.existsSync(defaultConfigPath)) {
+      console.error(
+        "\x1b[31m",
+        "can not find config file oss-publish.json",
+        "\x1b[m"
+      );
       process.exit(1);
-    }else {
+    } else {
       configPath = defaultConfigPath;
     }
-  }else {
-    configPath = path.join(options.config)
-    if(!fs.existsSync(configPath)){
-      console.error('\x1b[31m','can not find config file','\x1b[m');
+  } else {
+    configPath = path.join(options.config);
+    if (!fs.existsSync(configPath)) {
+      console.error("\x1b[31m", "can not find config file", "\x1b[m");
       process.exit(1);
     }
   }
   //找到了文件，读取
   let configJson = fs.readJsonSync(configPath);
-  configJson = checkConfigJson(configJson)
-  return configJson
+  configJson = checkConfigJson(configJson);
+  return configJson;
 }
 
-function checkConfigJson(configJson){
-  if(!configJson.type){
-    configJson.type = 'ali'
+function checkConfigJson(configJson) {
+  if (!configJson.type) {
+    configJson.type = "ali";
   }
 
-  if(!configJson.region){
-    console.error('\x1b[31m','can not find region params','\x1b[m');
+  if (!configJson.region) {
+    console.error("\x1b[31m", "can not find region params", "\x1b[m");
     process.exit(1);
   }
 
-  if(!configJson.bucket){
-    console.error('\x1b[31m','can not find bucket params','\x1b[m');
+  if (!configJson.bucket) {
+    console.error("\x1b[31m", "can not find bucket params", "\x1b[m");
     process.exit(1);
   }
 
-  if(!configJson.baseDir){
-    console.error('\x1b[31m','can not find baseDir params','\x1b[m');
+  if (!configJson.baseDir) {
+    console.error("\x1b[31m", "can not find baseDir params", "\x1b[m");
     process.exit(1);
   }
-  if(!fs.existsSync(configJson.baseDir)){
-    console.error('\x1b[31m','baseDir is not exist','\x1b[m');
+  if (!fs.existsSync(configJson.baseDir)) {
+    console.error("\x1b[31m", "baseDir is not exist", "\x1b[m");
     process.exit(1);
   }
 
-  return configJson
+  return configJson;
 }
-  
-function getAllOptions(){
-  const {akId,akSecret} = getAk();
+
+function getAllOptions() {
+  const { akId, akSecret } = getAk();
   const configJson = getOssOptions();
 
   return {
     ...configJson,
     accessKeyId: akId,
-    accessKeySecret: akSecret
-  }
+    accessKeySecret: akSecret,
+  };
 }
 
 const options = getAllOptions();
